9a652dc155872399a7dbc4a7e118f0016e159611
Scott Dancer <scottpdancer@gmail.com>
2021-07-25 19:14:16 -0700

    imported Resume.js and rendered it through App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React from 'react';
+import Resume from './Resume';
+// import hero from "./images/hero-comp.jpg"
 
 function App() {
   return (
-    <>
-    <div style = {{textAlign: "center"}} className="Home">
-      <h1 style={{ color: "#877D74"}}>
-        Scott Patrcick Dancer
-      </h1>
-      {/* <img src = {hero} className="hero" alt ="glowing laptop"></img> */}
-      {/* <img src = {FacePic} className="profilePic" alt ="headshot"></img> */}
-  
-      <div className="container">
-        <p>Full Stack Web Developer and Graphic Designer with a background in
-        illustration and cartooning. Possess a strong foundation in
-        communication, problem solving, and creativity that lends itself to
-        building applications that are aesthetically pleasing and meet
-        client expectations. Earned a Certificate in Web Development from
-        the University of Washington.</p>
-      <div className="contactInfo">
-        <p>Vancouver, WA | (360) 953-7969</p>
-          <p>Email:        scottpdancer@gmail.com</p>
-          <p>GitHub:      https://github.com/ScottDancer</p>
-          <p> Portfolio:   https://spd-react-profile.herokuapp.com/home</p>
-          <p> LinkedIn:   https://www.linkedin.com/in/scott-dancer-9744091b2/ </p>
-     
-      </div>
-    
-  
-      </div>
-    </div>
-
-    
-    </>
+   <Resume />
   )
 }
 
